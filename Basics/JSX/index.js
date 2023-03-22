f8bcffb4f69528f09e73138c8dafe1d1c8f57ff1
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-03-22 15:13:58 -0400

    converted the following to jsx

--- Basics/JSX/index.js
+++ Basics/JSX/index.js
@@ -1,36 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 let createElement = React.createElement
 
-let rootElement =
-  createElement('div', {className:'ContactList'}, 
-    createElement('h1', {className: 'ContactList-title'}, "Contacts"),
-    createElement('div', {},
-      createElement(
-        'div',
-        {className: 'Contact'},
-        createElement('div', {className:'Contact-avatar'}, "JN"),
-        createElement('span', {className:'Contact-name'}, "Myron Myron"),
-        createElement(
-          'a',
-          { href: 'mailto:mladyjen@uoguelph.ca', className: 'Contact-link'},
-          "mladyjen@uoguelph.ca",
-        ),
-        
-      ),
-      createElement(
-        'div',
-        {className: 'Contact'},
-        createElement('div', {className:'Contact-avatar'}, "M"),
-        createElement('span', {className:'Contact-name'}, "Me"),
-        createElement(
-          'a',
-          { href: 'mailto:me@example.com' , className:'Contact-link'},
-          "me@example.com"
-        )
-      )
-    )
-  )
+let rootElement = (
+  <div className='ContactList'>
+    <h1 className='ContactList-title'>Contacts</h1>
+    <div>
+      <div className='Contact'>
+        <div className='Contact-avatar'>JN</div>
+        <span className='Contact-name'>Myron Myron</span>
+        <a href='mailto:mladyjen@uoguleph.ca' className='Contact-link'>mladyjen@uoguelph.ca</a>
+      </div>
+      <div className='Contact'>
+        <div className='Contact-avatar'>DW</div>
+        <span className='Contact-name'>Myron Myron</span>
+        <a href='mailto:dwang11@uoguleph.ca' className='Contact-link'>dwang11@uoguelph.ca</a>
+      </div>
+    </div>
+  </div>
+)
 
 // The `ReactDOM` variable is set by the second `<script>` tag
 // in the above HTML file
-let domNode = document.getElementById('root')
-ReactDOM.render(rootElement, domNode)
+ReactDOM.render(rootElement, document.getElementById('root'))
