5b45167a28294e0bdb075e323a7e0b2114b9aea0
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-03-22 21:59:32 -0400

    finished the rendering of images

--- Basics/Conditionals/index.js
+++ Basics/Conditionals/index.js
@@ -1,24 +1,21 @@
 /*import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles.css'*/ //not needed for preview
-
-let billionaires = [
-  { name: 'Bill Gates', email: 'billg@microsoft.com' },
-  { name: 'Jeff Bezos', email: 'jeff@amazon.com' },
-  { name: 'Mark Zuckerberg', email: 'zuck@fb.com' },
-  { name: 'Myron Cringenko', email: 'mladyjen@uoguelph.ca'}
-]
-function getInitials({name, email}){
-  let final = '';
-  final.concat(name.split(' ').forEach(part =>{part.charAt(0)}));
-  return final;
-}
+var billionaires = require('./billionares.js')
+billionaires.forEach(element => {
+  console.log(element)
+});
 // Your React elements will go here.
 let elements = []
 billionaires.forEach((element, i) => {
+  let photo
+  if("photoURL" in element){
+    photo = <img src={element.photoURL}/>
+  }
   let contact = <div className='Contact' key={i}>
       <div className='Contact-avatar'>
         {element.name.split(' ').map(s => s[0]).join('')}
+        {photo}
       </div>
       <span className='Contact-name'>{element.name}</span>
       <a href={element.email}>
