108f4d8ec94ee3564fa56b601dfa4e4a3e042068
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2023-03-23 16:29:03 -0400

    implemented the contact component

--- Components/Intro/index.js
+++ Components/Intro/index.js
@@ -8,22 +8,37 @@ let billionaires = [
   { name: 'Mark Zuckerberg', email: 'zuck@fb.com' },
   { name: 'Myron Cringenko', email: 'mladyjen@uoguelph.ca'}
 ]
-
-
-// Your React elements will go here.
-let elements = []
-billionaires.forEach((element, i) => {
-  let contact = <div className='Contact' key={i}>
+function Contact(props){
+  let names = props.name.split(' ')
+  let initials = names.map(name => name[0].toUpperCase()).join('')
+  let photo
+  if("photoURL" in props){
+    photo = <img src={props.photoURL}/>
+  }
+  return (
+    <div className='Contact' key={props.i}>
       <div className='Contact-avatar'>
-        {element.name.split(' ').map(s => s[0]).join('')}
+        {initials}
+        {photo}
       </div>
-      <span className='Contact-name'>{element.name}</span>
-      <a href={element.email}>
-        {element.email}
+      <span className='Contact-name'>
+        {props.name}
+      </span>
+      <a href={props.email}>
+        {props.email}
       </a>
     </div>
+  );
+}
+
+// Your React elements will go here.
+let elements = billionaires.map(billionaire => 
+  <Contact {...billionaire} key={billionaire.email}/>)
+  
+/*billionaires.forEach((element, i) => {
+  let contact = <Contact {...billionaires} i/>
   elements.push(contact)
-})
+})*/
 
 // Use a for loop or array.map to build the elements array
 
